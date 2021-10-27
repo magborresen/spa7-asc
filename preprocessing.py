174aa18fa86bcc4a765af4b2bc979f893ed7c043
mabo-gomspace <mabo@gomspace.com>
2021-10-27 14:01:26 +0200

    added function to find data

--- preprocessing.py
+++ preprocessing.py
@@ -1,24 +1,84 @@
+import os
+import glob
+import re
 from scipy.io import wavfile
 from scipy import signal
-from matplotlib import pyplot as plt
-import numpy as np
+
 
 class preprocess():
     def __init__(self, path):
+        """ Intitialize the object with path to the training data
+
+        Args:
+            path (String): Path to training data folder
+
+        Returns:
+            no value
+        """
+
         self.path = path
-        self.samplerate, self.data = wavfile.read(self.path)
-        self.times = np.arange(len(self.data))/float(self.samplerate)
+        self.classes = ['office', 'outside', 'semi_outside', 'inside', 'inside_vehicle']
 
-    def spectrogram(self, nfft=1024, noverlap=512, window="hann"):
+    def make_training_data(self, method="spectrogram"):
+        """ Finds all training data and labels classes
 
-        # Spectrogram for channel 0
-        ch0f, ch0t, ch0Sxx = signal.spectrogram(self.data[:,0], self.samplerate,
-                                        nfft=nfft, noverlap=noverlap, window=window)
+        The functio finds all training data and labels the classes.
+        It will also preprocess the data into the given transform
 
-        ch1f, ch1t, ch1Sxx = signal.spectrogram(self.data[:,0], self.samplerate,
-                                        nfft=nfft, noverlap=noverlap, window=window)
+        Args:
+            method (String): Preprocessing technique to use
 
-        return ch0Sxx, ch1Sxx
+        Returns:
+            collected_data (list): List of preprocessed collected data
+            class_labels (list): List of class labels
+        """
+        files = glob.glob(os.path.join(self.path, '/**/*.wav'), recursive=True)
+        collected_data = []
+        class_labels = []
+        for f in files:
+            # Get the class
+            sample_class = [c for c in self.classes if re.search(r'\b' + c + r'\b', f)]
+            sample_rate, data = wavfile.read(f)
+            if method == "spectrogram":
+                transform = self.spectrogram(data, sample_rate)
+
+                collected_data.append(transform)
+                class_labels.append(sample_class)
+
+        return collected_data, class_labels
+
+    def spectrogram(self, data, sample_rate,  nfft=1024, noverlap=512, window="hann"):
+        """ Compute the spectrogram of a given signal
+
+        This will compute the spectrogram for both channels of the signal.
+
+        Args:
+            data (array): Sampled data
+            Fs (int): Sample rate
+            nfft (int): fft bin size
+            noverlap (int): numer of samples to overlap
+            window (String): Window function to use
+
+        Returns:
+            Spectrogram of both channels as ndarray
+        """
+        # If 2 channels
+        if data.shape[1] > 1:
+            # Spectrogram for channel 0
+            ch0f, ch0t, ch0Sxx = signal.spectrogram(data[:,0], sample_rate,
+                                            nfft=nfft, noverlap=noverlap, window=window)
+
+            ch1f, ch1t, ch1Sxx = signal.spectrogram(data[:,1], sample_rate,
+                                            nfft=nfft, noverlap=noverlap, window=window)
+
+            return ch0Sxx, ch1Sxx
+        # If 1 channel
+        else:
+
+            ch0f, ch0t, ch0Sxx = signal.spectrogram(data, sample_rate,
+                                            nfft=nfft, noverlap=noverlap, window=window)
+
+            return ch0Sxx
 
     def melSpec(self):
         return None
