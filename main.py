d919033d46640fdfabad63f4767997761e14644a
Magnus Borresen <magge4@yahoo.dk>
2021-11-01 10:21:05 +0000

    tweaks

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ warnings.filterwarnings("ignore", '.*Chunk*.')
 dirname = os.path.dirname(__file__)
 training_folder = os.path.join(dirname, "training_data/")
 
-# Use dirname in prep object when running on the server. Otherwise, insert an absolute path to the training data folder
-prep = preprocess("C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/")
+# Use training_folder in prep object when running on the server. Otherwise, insert an absolute path to the training data folder
+prep = preprocess(training_folder)
 
 data, classes = prep.make_training_data()
-
-print(len(classes))
