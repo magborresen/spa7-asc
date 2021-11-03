c0d3f3fc23caa21b68c8603ea685e6c87bf92565
MKampitakis <mike_kam@hotmail.com>
2021-11-03 14:35:55 +0100

    cnn_model with problem

--- cnn_model.py
+++ cnn_model.py
@@ -1,7 +1,6 @@
 """
 This file implements all model procedures
 """
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #this line removes all GPU usage warnings
 from os import listdir
@@ -18,15 +17,48 @@ from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras.models import Sequential
 
+def find_unique_classe(full_list):
+    """ finds the unique classes out of a class list
+
+        for use with preprocessing output before fitting into model
+
+        Args:
+            full_list (array): the 'classes' output of preprocesing
+
+        Returns:
+            list of unique classes
+    """
+    unique_list = [] 
+    for class_name in full_list:
+        # check if exists in unique_list or not
+        if class_name not in unique_list:
+            unique_list.append(class_name)
+    return(unique_list)
+
 class cnn_model:
+    """ initializes a CNN classifier model
+
+        Args:
+            data (array): a list of numpy arrays with spectogram data
+            classes (array): the corresponding class name of each spectogram
+
+        Returns:
+            no value
+    """
     def __init__(self, data, classes):
         self.data = np.array(data)
-        self.classes = np.array(classes)
-        self.ClassesNum = len(classes)
+        self.classes_all = np.array(classes)
+        self.classes = find_unique_classe(classes)
+        self.ClassesNum = len(self.classes)
+        image_data = np.array(data[50]) # should work with zero later
+        self.image_size = image_data.shape
+        print("image size -->", self.image_size)
+
+        print('what I feed into rescaling', self.image_size[0])
     
     def make_model(self):
         self.model = Sequential()
-        self.model.add(Rescaling(1./253))
+        #self.model.add(Rescaling(1./self.image_size[0])) TODO
         self.model.add(Conv2D(32, (3, 3), padding='same',
                          input_shape=(64,64,3)))
         self.model.add(BatchNormalization()) # I added this extra
@@ -62,8 +94,8 @@ class cnn_model:
     
     def train_model(self, model_name = "new_model", epoch = 50):
         self.epoch = epoch
-        (self.trainX, self.testX, self.trainY, self.testY) = train_test_split(self.data, self.classes,
-	        test_size=0.25, stratify=self.classes, random_state=42)
+        (self.trainX, self.testX, self.trainY, self.testY) = train_test_split(self.data, self.classes_all,
+	        test_size=0.25, stratify=self.classes_all, random_state=42)
         #self.dataset_train = tf.data.Dataset.from_tensor_slices(self.data) #might don't work
         #self.dataset = self.dataset.map(lambda x, y: x) TODO
         # initialize the training data augmentation object
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dirname = os.path.dirname(__file__)
 training_folder = os.path.join(dirname, "training_data/")
 
 # Use dirname in prep object when running on the server. Otherwise, insert an absolute path to the training data folder
-prep = preprocess("C:/Users/mike_/Aalborg Universitet/P7 - SPA7 - Documents/Project/SPA 7 770 database")
+prep = preprocess("C:/Users/mike_/OneDrive/Desktop/KampitakisCode/spa test/")
 # "C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/"
 
 data, classes = prep.make_training_data()
@@ -18,6 +18,5 @@ data, classes = prep.make_training_data()
 # CNN model
 spa_cnn_model = cnn_model(data, classes)
 spa_cnn_model.make_model()
-spa_cnn_model.train_model(model_name = "SPA_model", epoch = 4)
+#spa_cnn_model.train_model(model_name = "SPA_model", epoch = 4)
 
-print(len(classes))
