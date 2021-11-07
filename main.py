7209c9fb994abb3de5cfb146bb62cb91b287cfd7
Magnus Borresen <mb@magnusborresen.com>
2021-11-07 17:44:39 +0100

    changed frequency scale in spectrogram

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ warnings.filterwarnings("ignore", '.*Chunk*.')
 dirname = os.path.dirname(__file__)
 training_folder = os.path.join(dirname, "training_data/")
 
+<<<<<<< Updated upstream
 # Use training_folder in prep object when running on the server. Otherwise, insert an absolute path to the training data folder
 prep = preprocess("C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database")
 
+=======
+# Use dirname in prep object when running on the server. Otherwise, insert an absolute path to the training data folder
+prep = preprocess("C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/")
+# "C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/"
+# "C:/Users/mike_/OneDrive/Desktop/KampitakisCode/spa test/"
+>>>>>>> Stashed changes
 data, classes = prep.make_training_data()
 
 print(len(data), len(classes))
