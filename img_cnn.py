f79f02a9244ad49c64f275ffe00b95f8bacc10d2
Magnus Borresen <mb@magnusborresen.com>
2021-11-10 13:45:35 +0100

    split files

--- img_cnn.py
+++ img_cnn.py
@@ -1,23 +1,13 @@
 import warnings
 warnings.filterwarnings("ignore")
-import matplotlib
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing.image import NumpyArrayIterator
 from sklearn.metrics import confusion_matrix
-import itertools
-import os
-import shutil
-import random
-import glob
-import matplotlib.pyplot as plt
-import warnings
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import warnings
-from preprocessing import preprocess
 from img_cnn import CNN
 #from cnn_model import cnn_model
 
@@ -8,27 +7,8 @@ warnings.filterwarnings("ignore", '.*Chunk*.')
 
 
 dirname = os.path.dirname(__file__)
-training_folder = os.path.join(dirname, "training_data/")
 
-# Check if training folder/data already exists
 training_img_dir = os.path.join(dirname, "training")
-if not os.path.exists(training_img_dir):
-    # Use dirname in prep object when running on the server. Otherwise, insert an absolute path to the training data folder
-    prep = preprocess("C:/Users/kissg/Desktop/Training")
-    # "C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/"
-    # "C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/"
-    # "C:/Users/mike_/OneDrive/Desktop/KampitakisCode/spa test/"
-    prep.make_training_data()
-
-# CNN model
-#spa_cnn_model = cnn_model(data, classes, im_norm_size=(513, 860))
-#test_data, test_labels = spa_cnn_model.make_test_data(10)
-#spa_cnn_model.make_model()
-#spa_cnn_model.model_summary() # uncoment to check model node structure
-#spa_cnn_model.train_model(model_name = "SPA_model", epoch = 2)
-
-#predictions = spa_cnn_model.predict(test_data)
-#spa_cnn_model.comf_matrix(predictions, test_labels)
 
 
 
