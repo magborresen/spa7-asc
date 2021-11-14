1f180bfdb61f36e7adf4e667ee62c7ddbe69d63f
Magnus Borresen <mb@magnusborresen.com>
2021-11-14 21:41:14 +0100

    restructured

--- preprocessing.py
+++ preprocessing.py
@@ -18,7 +18,7 @@ warnings.filterwarnings("error")
 
 
 class preprocess():
-    def __init__(self, path):
+    def __init__(self, path, chunk_size=10):
         """ Intitialize the object with path to the training data
 
         Args:
@@ -28,11 +28,26 @@ class preprocess():
             no value
         """
 
-        self.path = path
-        self.dirname = os.path.dirname(__file__)
-        self.classes = ['office', 'outside', 'semi_outside', 'inside', 'inside_vehicle']
+        self._path = path
+        self._dirname = os.path.dirname(__file__)
+        self._classes = ['office', 'outside', 'semi_outside', 'inside', 'inside_vehicle']
+        self._audio_files = glob.glob(os.path.join(self._path, '**/*.wav'), recursive=True)
+        self._sample_rate = None
+        self._chunk_size = chunk_size
+        self.train_data = None
+        self.train_labels = None
+        self.train_img = []
+        self.vali_data = None
+        self.vali_labels = None
+        self.vali_img = []
+        self.test_data = None
+        self.test_labels = None
+        self.test_img = []
+        self.noise_data = None
+        self.noise_labels = None
+        self.noise_img = []
 
-    def make_training_data(self, method="spectrogram", chunk_size=10, 
+    def make_training_data(self, method="spectrogram", add_noise=None,
                             save_img=True, test_size=0.1, vali_size=0.1):
 
         """ Finds all training data and labels classes
@@ -45,7 +60,6 @@ class preprocess():
         Args:
             method (String): Preprocessing technique to use
             chunk_size (int): Chunk size to split each file into
-            save_img (bool): Whether to save the data as images or return them as arrays
             test_size (float): Size of test data given in percentage between 0 and 1
             vali_size (float): Size of validation data given in percentage between 0 and 1
 
@@ -54,11 +68,9 @@ class preprocess():
             class_labels (list): List of class labels
         """
 
-        audio_files = glob.glob(os.path.join(self.path, '**/*.wav'), recursive=True)
-        collected_data = []
         class_labels = []
         print("\nPreprocessing data into " + method + " data\n")
-        pbar = tqdm(audio_files)
+        pbar = tqdm(self._audio_files)
         for af in pbar:
             # Progress bar, just for show
             processing_name = af.split("/")
@@ -69,55 +81,101 @@ class preprocess():
             label_file = glob.glob(dirname + "/*.txt")
             
             # Get the class
-            sample_class = [c for c in self.classes if re.search(r'\b' + c + r'\b', af)]
+            sample_class = [c for c in self._classes if re.search(r'\b' + c + r'\b', af)]
 
-            data, sample_rate = sf.read(af)
+            data, self._sample_rate = sf.read(af)
             if len(label_file) > 0:
-                data = self.rm_labeled_noise(data, sample_rate, label_file[0])
+                data = self.rm_labeled_noise(data, label_file[0])
+
 
             # Create Chunks
-            data_chunks = self.chunk_file(data, sample_rate, chunk_size)
+            data_chunks = self.chunk_file(data)
 
+            # Create matching labels for the chunks
             for chunk in data_chunks:
-                
-                try:
-                    if method == "spectrogram":
-                        transform = self.spectrogram(chunk, sample_rate)
-                except UserWarning:
-                    continue
-
                 class_labels.append(sample_class[0])
 
-                # Check if returned data is two channels
-                if type(transform) == list:
-                    collected_data.append(transform[0])
-                    collected_data.append(transform[1])
-                    class_labels.append(sample_class[0])
-                else:
-                    collected_data.append(transform)
+            # Split training and validation data
+            self.train_data, self.vali_data, self.train_labels, self.vali_labels = train_test_split(data_chunks, 
+                                                                                class_labels,
+                                                                                test_size=vali_size,
+                                                                                random_state=42)
 
-        # Split training and validation data
-        train_data, vali_data, train_labels, vali_labels = train_test_split(collected_data, 
-                                                                            class_labels,
-                                                                            test_size=vali_size,
-                                                                            random_state=42)
+            # Split training and test data based on the previous split.
+            self.train_data, self.test_data, self.train_labels, self.test_labels = train_test_split(self.train_data, 
+                                                                                self.train_labels,
+                                                                                test_size=test_size,
+                                                                                random_state=42)
 
-        # Split training and test data based on the previous split.
-        train_data, test_data, train_labels, test_labels = train_test_split(train_data, 
-                                                                            train_labels,
-                                                                            test_size=test_size,
-                                                                            random_state=42)
+            # Convert test data using selected noise and image model
+            for d in self.test_data:
+                if add_noise == "awgn":
+                    y = self.awgn(d)
+                if method.lower() == "spectrogram":
+                    self.test_img.append(self.spectrogram(y))
 
-        if save_img:
-            self.save_as_img(train_data, train_labels, 'training')
-            self.save_as_img(vali_data, vali_labels, 'vali')
-            self.save_as_img(test_data, test_labels, 'test')
-            return None
+            # Convert training data using the selected method
+            for d in self.train_data:
+                if method.lower() == "spectrogram":
+                    self.train_img.append(self.spectrogram(d))
 
-        else:
-            return collected_data, class_labels
+            # Convert validation data using the selected method
+            for d in self.vali_data:
+                if method.lower() == "spectrogram":
+                    self.vali_img.append(self.spectrogram(d))
 
-    def rm_labeled_noise(self, data, sample_rate, label_file_path):
+            # Save as images of selected
+            if save_img:
+                self.save_as_img(data_type="training")
+                self.save_as_img(data_type="validation")
+                self.save_as_img(data_type="test")
+
+        return True
+
+    def make_env_noise(self, method="spectrogram", save_img=True):
+
+        collected_noise = []
+        class_labels = []
+        print("\nPreprocessing data into " + method + " data\n")
+        pbar = tqdm(self._audio_files)
+        for af in pbar:
+            # Progress bar, just for show
+            processing_name = af.split("/")
+            pbar.set_description("Processing %s" % processing_name[-1] + " in " + processing_name[-2])
+
+            # Find associated label files
+            dirname = os.path.dirname(os.path.abspath(af))
+            label_file = glob.glob(dirname + "/*.txt")
+            
+            # Get the class
+            sample_class = [c for c in self._classes if re.search(r'\b' + c + r'\b', af)]
+
+            data, self._sample_rate = sf.read(af)
+            if len(label_file) > 0:
+                noise = self.get_labeled_noise(data, label_file[0])
+
+            # Create Chunks
+            noise_chunks = self.chunk_file(noise, self._sample_rate, self._chunk_size)
+
+            # Convert data using the given method
+            for chunk in noise_chunks:
+                class_labels.append(sample_class[0])
+                if method.lower() == "spectrogram":
+                    transform = self.spectrogram(self.noise_data)
+                    self.noise_img.append(transform)
+                collected_noise.append(chunk)
+
+            self.noise_data = collected_noise
+            self.noise_labels = class_labels
+
+            # Save as images if selected. 
+            if save_img:
+                self.save_as_img(data_type="noise")
+
+        return True
+
+
+    def rm_labeled_noise(self, data, label_file_path):
         cut_data_ch0 = np.array([])
         cut_data_ch1 = np.array([])
 
@@ -141,50 +199,85 @@ class preprocess():
 
         return cut_data
 
-    def chunk_file(self, data, sample_rate, chunk_size):
+    def get_labeled_noise(self, data, label_file_path):
+        noise_data_ch0 = np.array([])
+        noise_data_ch1 = np.array([])
+
+        with open(label_file_path, encoding="utf8") as f:
+            lines = f.readlines()
+
+        for line in lines:
+            time_labels = line.split('\t')
+            start_time = int(float(time_labels[0]) * 44100)
+            stop_time = int(float(time_labels[1]) * 44100)
+            # Check if data is two channel
+            if data.ndim > 1:
+                noise_data_ch0 = np.append(noise_data_ch0, data[:,0][start_time:stop_time])
+                noise_data_ch1 = np.append(noise_data_ch1, data[:,1][start_time:stop_time])
+                noise_data = np.vstack((noise_data_ch0, noise_data_ch1)).T
+            else:
+                noise_data_ch0 = np.append(noise_data_ch0, data[start_time:stop_time])
+                noise_data = noise_data_ch0
+
+        return noise_data
+
+    def chunk_file(self, data):
         chunk_data = []
         start_sample = 0
-        samples_per_chunk = chunk_size * sample_rate
+        samples_per_chunk = self._chunk_size * self._sample_rate
 
         for sc in range(math.floor(len(data) / samples_per_chunk)):
-            stop_sample = sc * chunk_size * sample_rate
+            stop_sample = sc * self._chunk_size * self._sample_rate
             # Check if data is two channel
             if data.ndim > 1:
                 chunk_ch0 = data[:,0][start_sample:stop_sample]
+                chunk_data.append(chunk_ch0)
                 chunk_ch1 = data[:,1][start_sample:stop_sample]
-                chunk = np.vstack((chunk_ch0, chunk_ch1)).T
+                chunk_data.append(chunk_ch1)
             else:
                 chunk = data[start_sample:stop_sample]
-            chunk_data.append(chunk)
+                chunk_data.append(chunk)
             start_sample = stop_sample
 
         return chunk_data
 
-    def save_as_img(self, data, classes, data_type):
+    def save_as_img(self, data_type=None):
         """ Saves the given data as images
 
         The function matches the class data to the training data
         and gives the saved png file as the class name plus some number
 
         Args:
-            data (array): Spectrogram image data 2D
-            classes (array): Classes with index corresponding to the data
             data_type (String): Which main folder to save the data to e.g. training
 
         Returns:
             None
         """
+        if data_type == "training":
+            data = self.train_img
+            classes = self.train_labels
+        elif data_type == "validation":
+            data = self.vali_img
+            classes = self.vali_labels
+        elif data_type == "test":
+            data = self.test_img
+            classes = self.test_labels
+        elif data_type == "noise":
+            data = self.noise_img
+            classes = self.noise_labels
+        else:
+            print("No datatype given, no data will be saved")
+            return False
+
         print(f"Saving {data_type} data as images")
         for i in tqdm(range(len(data))):
-            filedir = os.path.join(self.dirname, data_type, classes[i])
+            filedir = os.path.join(self._dirname, data_type, classes[i])
             if not os.path.exists(filedir):
                 os.makedirs(filedir)
-            filename = os.path.join(self.dirname, data_type, classes[i], classes[i] + f"{i}.png")
+            filename = os.path.join(self._dirname, data_type, classes[i], classes[i] + f"{i}.png")
             imsave(filename, data[i])
 
-
-
-    def spectrogram(self, data, sample_rate,  nperseg=1024, noverlap=512, window="hann"):
+    def spectrogram(self, data, nperseg=1024, noverlap=512, window="hann"):
         """ Compute the spectrogram of a given signal
 
         This will compute the spectrogram for both channels of the signal.
@@ -199,28 +292,14 @@ class preprocess():
         Returns:
             Spectrogram of both channels as ndarray
         """
-        # If 2 channels
-        if data.ndim > 1:
-            # Spectrogram for channel 0
-            ch0f, ch0t, ch0Sxx = signal.spectrogram(data[:,0], sample_rate,
-                                            nperseg=nperseg, noverlap=noverlap, window=window)
 
-            ch1f, ch1t, ch1Sxx = signal.spectrogram(data[:,1], sample_rate,
-                                            nperseg=nperseg, noverlap=noverlap, window=window)
+        f, t, Sxx = signal.spectrogram(data, self._sample_rate,
+                                        nperseg=nperseg, noverlap=noverlap, window=window)
 
-            try:
-                return [10*np.log10(ch0Sxx), 10*np.log10(ch1Sxx)]
-            except RuntimeWarning:
-                try:
-                    return 10*np.log10(ch0Sxx)
-                except RuntimeWarning:
-                    return 10*np.log10(ch1Sxx)
-
-        # If 1 channel
-        ch0f, ch0t, ch0Sxx = signal.spectrogram(data, sample_rate,
-                                            nperseg=nperseg, noverlap=noverlap, window=window)
-
-        return 10*np.log10(ch0Sxx)
+        try:
+            return 10*np.log10(Sxx)
+        except RuntimeWarning:
+            pass
 
     def mel_spectrogram(self, data, sample_rate, nfft=1024, startframe=0):
         """ Compute the mel spectrogram of a given signal
@@ -327,3 +406,8 @@ class preprocess():
         ch0Z = squareform(ch0dist)
 
         return ch0Z
+
+    def awgn(self, x):
+        wgn = np.random.normal(loc=0.0, scale=1.0, size=x.shape[0])
+        return np.add(x, wgn)
+        
