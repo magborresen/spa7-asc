d12d34f38681ebc82414d4f779ec48d96d4d118e
MKampitakis <mike_kam@hotmail.com>
2021-11-16 15:07:32 +0100

    working: random, burst

--- run_preprocess.py
+++ run_preprocess.py
@@ -5,14 +5,15 @@ from preprocessing import preprocess
 warnings.filterwarnings("ignore", '.*Chunk*.')
 
 
-dirname = os.path.dirname(__file__)
+#dirname = os.path.dirname(__file__)
+dirname = "C:/Users/mike_/Desktop/KampitakisCode/spa test/"
 training_folder = os.path.join(dirname, "training_data/")
 
 # Check if training folder/data already exists
 training_img_dir = os.path.join(dirname, "training")
 if not os.path.exists(training_img_dir):
     # Use dirname in prep object when running on the server. Otherwise, insert an absolute path to the training data folder
-    prep = preprocess(training_folder)
+    prep = preprocess("C:/Users/mike_/Desktop/KampitakisCode/spa test/training_data")
     # "C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/"
     # "C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/"
     # "C:/Users/mike_/OneDrive/Desktop/KampitakisCode/spa test/"
