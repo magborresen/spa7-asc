c798b176b69fee156d050906460b283a18e9c5d4
Madsen <bmadse18@student.aau.dk>
2021-12-01 09:58:16 +0100

    Branch update

--- run_preprocess.py
+++ run_preprocess.py
@@ -12,9 +12,10 @@ training_folder = os.path.join(dirname, "training_data/")
 training_img_dir = os.path.join(dirname, "training")
 if not os.path.exists(training_img_dir):
     # Use dirname in prep object when running on the server. Otherwise, insert an absolute path to the training data folder
-    prep = preprocess("C:/Users/mike_/Desktop/KampitakisCode/spa test/training_data")
+    prep = preprocess("C:/Users/bjark/Documents/Uni/OneDrive/OneDrive - Aalborg Universitet/SPA7/Project/Training database")
     # "C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/"
     # "C:/Users/mabo/Aalborg Universitet/P7 - SPA7 - Dokumenter/Project/SPA 7 770 database/"
     # "C:/Users/mike_/Desktop/KampitakisCode/spa test/training_data"
-    prep.make_training_data(add_speech=True)
+    # "C:/Users/bjark/Documents/Uni/OneDrive/OneDrive - Aalborg Universitet/SPA7/Project/Training database"
+    prep.make_training_data(save_img=True)
     #prep.make_env_noise()
